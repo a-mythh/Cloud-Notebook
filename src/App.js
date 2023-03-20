93501f7b8d87710d109fd0e1ec4c9113071c6146
a-mythh <amitdas240802@gmail.com>
2023-03-20 10:34:15 +0530

    added navigation bar and react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from "react";
+
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <h1> This is Cloud Notebook </h1>
-        </div>
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="about/*" element={<About />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
