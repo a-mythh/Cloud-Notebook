687c0ee53e4e0a7f87b557e59ed4eb9aa08f8766
a-mythh <amitdas240802@gmail.com>
2023-03-20 11:03:45 +0530

    used Context API

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import Home from "./components/Home";
 import About from "./components/About";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="about/*" element={<About />} />
-                </Routes>
-            </BrowserRouter>
+            <NoteState>
+                <BrowserRouter>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="about/*" element={<About />} />
+                    </Routes>
+                </BrowserRouter>
+            </NoteState>
         </>
     );
 }
